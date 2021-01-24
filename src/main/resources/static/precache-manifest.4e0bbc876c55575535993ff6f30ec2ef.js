7f35443ded33cf4941763f3604f5ce3eae94f153
Scott Sinclair <scott.sinclair@extern.wrwks.at>
2021-01-24 22:11:53 +0100

    added static UI resources

---
+++ src/main/resources/static/precache-manifest.4e0bbc876c55575535993ff6f30ec2ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a78df53b07e5781cfe579e0df6d26d9c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b063fbf0f8306fe68d1",
+    "url": "/static/css/main.f03c1e97.chunk.css"
+  },
+  {
+    "revision": "57e9ae591eb61c2c3e32",
+    "url": "/static/js/2.d301c8b4.chunk.js"
+  },
+  {
+    "revision": "53dd0fe36f0c355508e58d4292509f68",
+    "url": "/static/js/2.d301c8b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b063fbf0f8306fe68d1",
+    "url": "/static/js/main.aeff2e02.chunk.js"
+  },
+  {
+    "revision": "063ff0f3230b6626294e",
+    "url": "/static/js/runtime-main.8d2ec4d5.js"
+  }
+]);
